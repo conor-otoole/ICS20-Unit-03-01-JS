d33ef57ae1a3f7b2585320e73b56e055ccbb4c1b
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2022-11-25 14:40:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Conor All rights reserved
 //
 // Created by: conor-otoole
-// Created on: Sep 2020
+// Created on: nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("ICS20-Unit-03-01-JSS", {
+    scope: "/ICS20-Unit-03-01-JSS/",
   })
 }
 
